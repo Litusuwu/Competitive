4a93b265850980e248dc7a9c14ffae6c7532ba8c
Litusuwu <carlos.zegarrab@pucp.edu.pe>
2025-01-28 12:06:40 -0500

    Tu me enseñaste a odiarte

--- B_Dynamic_Range_Sum_Queries.cpp
+++ B_Dynamic_Range_Sum_Queries.cpp
@@ -20,10 +20,61 @@ typedef vector<ll> vl;
 
 void setIO(string s);
 
-void Solve(){
-    int n, m, k, l, d, r, ans = 0;
-    
+vector<ll>v;
+vector<ll>segmentTree;
+void build(ll nodo, ll left, ll right){
+    if(left == right){
+        segmentTree[nodo] = v[left];
+    }
+    else{
+        ll mid = (left + right)/2;
+        build(nodo*2, left, mid);
+        build(nodo*2 + 1, mid + 1, right);
+        segmentTree[nodo] = segmentTree[nodo*2] + segmentTree[nodo*2 + 1];
+    }
 }
+ll suma(ll nodo, ll left, ll right, ll l, ll r){
+    if ( l > r ) return 0;
+    if ( l == left && r == right){
+        return segmentTree[nodo];
+    }
+    ll mid = (left + right)/2;
+    return suma(nodo*2, left, mid, l, min(r, mid)) +
+            suma(nodo*2 + 1, mid + 1, right, max(l, mid+1), r);
+}
+void update(ll v, ll left, ll right, ll pos, ll new_val) {
+    if (left == right) {
+        segmentTree[v] = new_val;
+    } else {
+        ll tm = (left + right) / 2;
+        if (pos <= tm){
+            update(v*2, left, tm, pos, new_val);
+        }
+        else{
+            update(v*2+1, tm+1, right, pos, new_val);
+        }
+        segmentTree[v] = segmentTree[v*2] + segmentTree[v*2+1];
+    }
+}
+void Solve(){
+    ll n, m, k, l, d, r, ans = 0;
+    cin >> n >> m;
+    v.resize(n);
+    segmentTree.resize(4*n + 1);
+    
+    rep(i, n) cin >> v[i];
+    build(1, 0, n-1);
+    rep(i, m){
+        cin >> k >> l >> r;
+        if(k == 1){
+            update(1, 0, n-1, l-1, r);
+        }
+        else{
+            cout << suma(1, 0, n-1, l-1, r-1)<< endl;
+        }
+    }
+}
+
 
 
 int main(){
